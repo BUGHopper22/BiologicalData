7582edb263ec721b8aca6200946cf87cc6c0154b
dparolo <34452998+dparolo@users.noreply.github.com>
2019-03-02 12:18:53 +0100

    updated adjustment

--- progetto/2-MSAClustal/2-msaAdjust2.py
+++ progetto/2-MSAClustal/2-msaAdjust2.py
@@ -4,36 +4,51 @@ from Bio.SeqRecord import SeqRecord
 from Bio.Align import MultipleSeqAlignment
 from Bio import AlignIO
 import numpy as np
+import matplotlib.pyplot as plt
 
-alignment = AlignIO.read(open("1-msa200.clw"), "clustal")
-# l è l'array costruito con ogni riga che corrisponde ad una sequenza
+alignment = AlignIO.read(open("msa200.clw"), "clustal")
+
 l = np.array([np.array(list(str(s.seq))) for s in alignment])
-# l=="-" crea una matrice booleana con 1 ogni volta che trova un "-", sum somma tutti gli uni per ogni colonna, e facendo /l.shape[0] trovo la percentuale di - sul totale delle righe. 
-# c sarà una matrice ad un vettore riga contenete le percentuali di "-" trovati per ogni colonna
+
 c = np.sum(l == "-", axis = 0)/l.shape[0]
 
-threshold = 0.1
+threshold_c = 0.1
 
-# creo un array contenente i numeri da zero fino al numero di colonne-1
 idx = np.arange(c.shape[0])
-# 
-idx = idx[c < threshold]
+idx = idx[c < threshold_c]
 
+c_x = c[idx]
+
+plt.plot(idx,c_x)
 new_l = l[:,idx]
+
+r = np.sum(new_l == "-", axis = 1)/new_l.shape[1]
+
+threshold_r = 0.06
+
+idx = np.arange(r.shape[0])
+idx = idx[r > threshold_r]
+
+new_l[idx,:] = '?' # check to not include undesired sequences
 
 seq_n = [None]*len(alignment)
 
 for i in range(len(alignment)):
-    s = list(new_l[i,:])
-    s = [str(el) for el in s]
-    seq_n[i] = "".join(s)
+    if i not in idx:
+        s = list(new_l[i,:])
+        s = [str(el) for el in s]
+        seq_n[i] = "".join(s)
 
-msa = [None]*len(alignment)
+msa_tmp = [None]*len(alignment)
+msa = []
 
 for i in range(len(alignment)):
-    msa[i] = SeqRecord(seq = Seq(seq_n[i],SingleLetterAlphabet()), id = alignment[i].id, 
-       name = alignment[i].name, description = alignment[i].description, dbxrefs = alignment[i].dbxrefs)
+    if i not in idx:
+        msa_tmp[i] = SeqRecord(seq = Seq(seq_n[i],SingleLetterAlphabet()), id = alignment[i].id, 
+         name = alignment[i].name, description = alignment[i].description, dbxrefs = alignment[i].dbxrefs)
+        msa.append(msa_tmp[i])
 
+    
 align = MultipleSeqAlignment(msa)
 
 out = open("align_cleaned.clw", "w")
