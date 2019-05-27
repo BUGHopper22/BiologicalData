d53df6ee32b69c0a12304dbd74214da12a8ba86c
Hopper <alberto.bacco.1@gmail.com>
2019-05-27 08:40:12 +0200

    funziona_push

--- progetto/1-Blast/UniprotSequenceRequest.py
+++ progetto/1-Blast/UniprotSequenceRequest.py
@@ -4,14 +4,16 @@ import sys
 import urllib.request
 
 # input: file ids.txt contenente tutti gli id delle proteine
-# output: file sequence.fasta con le sequenze delle proteine
+# output: file sequence.fasta con le sequenze delle proteine ()
+
 
 def idProteinList(idFile):
-    return [l[15:-1] for  l in open(idFile)] 
+    return [l[15:-1] for l in open(idFile)]
+
 
 def IdsToSequences(idList):
-    uniprotUrl='https://www.uniprot.org/uniprot/'
-    upiparcUrl='https://www.uniprot.org/uniparc/'
+    uniprotUrl = 'https://www.uniprot.org/uniprot/'
+    upiparcUrl = 'https://www.uniprot.org/uniparc/'
     # extension = '.fasta'
     with open('sequences.fasta', 'w') as output:
         for i in range(0, 200):
@@ -24,7 +26,8 @@ def IdsToSequences(idList):
                 singleProtein = urllib.request.urlopen(url).read()
                 output.write(singleProtein.decode("utf-8"))
 
+
 if __name__ == "__main__":
-    idFile='ids.txt'
-    idList=idProteinList(idFile)
+    idFile = 'ids.txt'
+    idList = idProteinList(idFile)
     IdsToSequences(idList)
